60e904610e52f625e9b1d6a91cbb21dd83fc519f
vichanley <viccantu23@gmail.com>
2022-01-30 22:24:00 -0600

    troubleshooting questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,52 @@
+//The Questions
+var questions = [
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
+    },
+    { 
+        question: "Which of the following function of Array object calls a function for each element in the array?", 
+        answers: [
+            { text: "concat()", correct: false },
+            { text: "filter()", correct: false },
+            { text: "forEach()", correct: true },
+            { text: "split()", correct: false }
+        ]
+    },
+    { 
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+        answers: [
+            { text: "if (i != 8)", correct: true },
+            { text: "if i =! 8", correct: false },
+            { text: "if (i <> 8)", correct: false },
+            { text: "if (i !=== 8)", correct: false }
+        ]
+    },
+    { 
+        question: "What is the correct way to write a JavaScript array?", 
+        answers: [
+            { text: "var vegetable = (0:'kale', 1:'lettuce', 2:'spinach')", correct: false },
+            { text: "var vegetable = ['kale', 'lettuce', 'spinach']", correct: true },
+            { text: "var vegetable = (kale, lettuce, spinach)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+    { 
+        question: "How do you round the number 102.456, to the nearest integer?",
+        answers: [
+            { text: "Math.random(102.456)", correct: false },
+            { text: "Math.rnd(102.456)", correct: false },
+            { text: "round(102.456)", correct: false },
+            { text: "None of the above", correct: true }
+        ]
+    },
+];
+
 var timeLeft = 75;
 var timerID;
 var timerEl = document.getElementById("timer");
@@ -13,9 +62,11 @@ var submitButton = document.getElementById("submit-btn");
 var clearButton = document.getElementById("clear-btn");
 var initialsField = document.getElementById("initials-field");
 var backButton = document.getElementById("back-btn");
+var scoreField = document.getElementById("player-score");
 var scores = JSON.parse(localStorage.getItem("scores"));
 var shuffledQuestions;
 var currentQuestionIndex;
+
 
 //Start & Next Button Event Listeners
 startButton.addEventListener("click", startQuiz);
@@ -43,11 +94,11 @@ function startQuiz() {
     questionPageEl.classList.remove("hide");
 
     countdownTimer();
-    nextQuestion();
+    setNextQuestion();
 }
 
 //Next Question function
-function nextQuestion()
+function setNextQuestion()
 {
     resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex])
@@ -64,13 +115,12 @@ function showQuestion(question) {
             button.dataset.correct = answer.correct
         }
         button.addEventListener("click", selectAnswer)
-        answerButtonsEl.appendChild(button)
+        answerButtonEl.appendChild(button)
     })
 };
 
 //Reset function 
 function resetState() {
-    //clearStatusClass(document.body)
     nextButton.classList.add("hide")
     checkAnswerEl.classList.add("hide")
     while (answerButtonEl.firstChild) {
